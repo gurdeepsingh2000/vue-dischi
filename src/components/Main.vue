e85b9c5487acd3ba711bcd96a157707740b49c9b
Gurdeep Singh <gurdeepsingh2000329@gmail.com>
2021-07-14 15:51:16 +0200

    update

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,30 @@
 <template>
 
-<div>ciao</div>  
+<section>
+    <div class="container">
+        <div class="row">
+            <div>
+                <Song
+                v-for= "(Song, index) in Songs" 
+                :key= "index"  
+                :poster= "Song.poster" 
+                :title= "Song.title" 
+                :author= "Song.author"
+                :year= "Song.year" 
+                />
+            </div>
+        </div>
+    </div>
+</section>
 
 </template>
 
 <script>
-import Song from "./components/Song.vue"
+import Song from "../components/Song.vue"
 export default {
     name:'Main',
     components: {
-        Song        //Componente Aggiunto
+        Song     //Componente Aggiunto
     },
     Props: {
         Songs: Array, //Regitriamo l'attributo Songs(Array di oggetti pescati dall'API) in Main.vue cosi da poterci lavorare sopra
---
+++ src/components/Song.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="song-container"> 
+        <div class="box">
+            <img :src="poster">
+            <span class="title">{{title}}</span>
+            <p class="author">{{author}}</p>
+            <p class="year">{{year}}</p>
+        </div>       
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Song",
+    props: {
+        poster: String,
+        title: String,
+        author: String,
+        year: String
+    }
+}
+</script>
